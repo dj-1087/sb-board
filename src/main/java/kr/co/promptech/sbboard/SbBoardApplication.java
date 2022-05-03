e2082331aa9dc33e2b0d92efd234bcf70bf9ba84
Dongjun Jeong <dj.jeong@promptech.co.kr>
2022-05-04 03:23:14 +0900

    BaseEntity, BaseTimeEntity 추가

--- src/main/java/kr/co/promptech/sbboard/SbBoardApplication.java
+++ src/main/java/kr/co/promptech/sbboard/SbBoardApplication.java
@@ -2,6 +2,10 @@ package kr.co.promptech.sbboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class SbBoardApplication {
@@ -10,4 +14,9 @@ public class SbBoardApplication {
 		SpringApplication.run(SbBoardApplication.class, args);
 	}
 
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		// TODO: 추후 username으로 변경
+		return () -> Optional.of("dj-1087");
+	}
 }
