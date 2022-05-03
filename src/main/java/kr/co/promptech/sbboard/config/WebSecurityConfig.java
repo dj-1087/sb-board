4c1862d85aaac6ea00b3fc888fae4668788ddd82
Dongjun Jeong <dj.jeong@promptech.co.kr>
2022-05-04 03:26:33 +0900

    WebSecurityConfig 요청 권한 설정

---
+++ src/main/java/kr/co/promptech/sbboard/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package kr.co.promptech.sbboard.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers( "/css/**", "/extension/**", "/fonts/**", "/icons/**", "/images/**", "/js/**", "/node_modules/**", "/sass/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/auth/**").permitAll() // 누구나 접근 허용
+                .antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
+                .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
+                .and()
+                .formLogin()
+                .loginPage("/auth/login") // 로그인 페이지 링크
+                .defaultSuccessUrl("/") // 로그인 성공 후 리다이렉트 주소
+                .and()
+                .logout()
+                .logoutSuccessUrl("/auth/login") // 로그아웃 성공시 리다이렉트 주소
+                .invalidateHttpSession(true) // 세션 날리기
+        ;
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+
+}
