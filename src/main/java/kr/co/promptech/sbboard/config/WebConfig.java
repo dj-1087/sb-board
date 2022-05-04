5fd3b981dbf3d6f266d173990322ae8a5d30b11f
Dongjun Jeong <dj.jeong@promptech.co.kr>
2022-05-04 11:19:39 +0900

    ModelMapper 추가

---
+++ src/main/java/kr/co/promptech/sbboard/config/WebConfig.java
@@ -0,0 +1,15 @@
+package kr.co.promptech.sbboard.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.NameTokenizers;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
