f727a7af06bb97a8870ae3ed1aea42be5ec80786
Dongjun Jeong <dj.jeong@promptech.co.kr>
2022-05-05 20:34:35 +0900

    회원가입 뷰 기초 작업

--- src/main/java/kr/co/promptech/sbboard/controller/AccountController.java
+++ src/main/java/kr/co/promptech/sbboard/controller/AccountController.java
@@ -1,10 +1,14 @@
 package kr.co.promptech.sbboard.controller;
 
+import kr.co.promptech.sbboard.model.dto.AccountDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/auth")
@@ -12,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AccountController {
     @GetMapping("/sign-up")
     public String signUpView(Model model) {
-
+        model.addAttribute("accountDto", new AccountDto());
         return "app/auth/signUp";
     }
 }
---
+++ src/main/java/kr/co/promptech/sbboard/model/dto/AccountDto.java
@@ -0,0 +1,32 @@
+package kr.co.promptech.sbboard.model.dto;
+
+import kr.co.promptech.sbboard.model.Account;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountDto {
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    private String nickname;
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "수신가능한 이메일을 입력하세요.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+            message = "영문 대,소문자와 숫자, 특수문자 모두 조합하여 8~20자리 이상으로 비밀번호를 설정해주세요.")
+    private String password;
+
+    // TODO: 추후 Enum으로 수정
+    private String accountType;
+}
