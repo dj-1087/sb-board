07e8c9fc219e54cf56f7da1a533f0eb9c1140ab7
Dongjun Jeong <dj.jeong@promptech.co.kr>
2022-05-11 18:12:05 +0900

    UI 수정
    - home
    - login
    - sign-up

--- src/main/java/kr/co/promptech/sbboard/controller/CommentController.java
+++ src/main/java/kr/co/promptech/sbboard/controller/CommentController.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
