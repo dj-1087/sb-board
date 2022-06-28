08260455621ec92c5f96149f1ed4314049fc0a31
Dongjun Jeong <dj.jeong@promptech.co.kr>
2022-06-28 14:50:51 +0900

    로딩 뷰 추가

--- src/main/resources/static/js/utils/loading-handler.js
+++ src/main/resources/static/js/utils/loading-handler.js
@@ -1,5 +1,22 @@
 class LoadingHandler {
     constructor() {
+        this.defaultLoadingView = Handlebars.getTemplate("loading");
+        this.loadingOverlay = undefined;
+    }
+
+    initLoadingView() {
+        const mainContentDiv = document.querySelector(".main-content");
+        mainContentDiv.innerHTML += this.defaultLoadingView()
+
+        this.loadingOverlay = document.querySelector('.loading-overlay')
+    }
+
+    start() {
+        this.loadingOverlay.classList.add('is-active')
+    }
+
+    end() {
+        this.loadingOverlay.classList.remove('is-active')
     }
     
     /*로딩 버튼 안에 로딩전에 표시할 element와 로딩 중에 표시될 element가 있어야 함
